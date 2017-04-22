09873feb377e6972fd8d45b463b49e91c5fd3501
Jrogerson74 <jrsquish@gmail.com>
2017-04-22 01:23:35 -0600

    Added guest user for current_user

--- app/controllers/concerns/set_source.rb
+++ app/controllers/concerns/set_source.rb
@@ -1,11 +1,11 @@
 module SetSource
   extend ActiveSupport::Concern
- 
-  included do |variable|
+
+  included do
     before_filter :set_source
   end
- 
+
   def set_source
     session[:source] = params[:q] if params[:q]
-  end 
- end
+  end
+end
